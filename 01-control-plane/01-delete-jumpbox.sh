13728c08ec0115394321d5e0bd8b6b30abfda0ca
Clayton Hynfield <clayton@hynfield.org>
2019-07-03 15:57:43 -0400

    Make jumpbox creation and deletion idempotent

--- 01-control-plane/01-delete-jumpbox.sh
+++ 01-control-plane/01-delete-jumpbox.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-gcloud compute instances delete 'jbox-pks' \
-    --project "$GCP_PROJECT_ID" \
-    --zone "$GCP_ZONE" \
-    --delete-disks 'all' \
-    --quiet
+jumpbox_exists() {
+    gcloud compute instances list \
+        --project "$GCP_PROJECT_ID" \
+        --filter "name=('jbox-pks') AND zone:('$GCP_ZONE')" \
+        | grep 'jbox-pks.*RUNNING' \
+        >/dev/null 2>&1
+}
+
+if jumpbox_exists; then
+    gcloud compute instances delete 'jbox-pks' \
+        --project "$GCP_PROJECT_ID" \
+        --zone "$GCP_ZONE" \
+        --delete-disks 'all' \
+        --quiet
+fi
--- 01-control-plane/01-provision-jumpbox.sh
+++ 01-control-plane/01-provision-jumpbox.sh
@@ -3,11 +3,22 @@
 gcloud services enable compute.googleapis.com \
     --project "$GCP_PROJECT_ID"
 
-gcloud compute instances create 'jbox-pks' \
-    --image-project 'ubuntu-os-cloud' \
-    --image-family 'ubuntu-1804-lts' \
-    --boot-disk-size '200' \
-    --machine-type='g1-small' \
-    --project "$GCP_PROJECT_ID" \
-    --zone "$GCP_ZONE" \
-    --tags "jbox-pks"
+jumpbox_exists() {
+    gcloud compute instances list \
+        --project "$GCP_PROJECT_ID" \
+        --filter "name=('jbox-pks') AND zone:('$GCP_ZONE')" \
+        | grep 'jbox-pks.*RUNNING' \
+        >/dev/null 2>&1
+}
+
+if ! jumpbox_exists; then
+    gcloud compute instances create 'jbox-pks' \
+        --image-project 'ubuntu-os-cloud' \
+        --image-family 'ubuntu-1804-lts' \
+        --boot-disk-size '200' \
+        --machine-type='g1-small' \
+        --project "$GCP_PROJECT_ID" \
+        --zone "$GCP_ZONE" \
+        --tags "jbox-pks"
+fi
+
